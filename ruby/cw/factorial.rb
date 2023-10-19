dbc88057224d1901fb31c11858a4f18e1d86d184
Kyryl <91323838+kirizdev@users.noreply.github.com>
2023-10-19 22:59:15 +0000

    Create factorial.rb

---
+++ ruby/cw/factorial.rb
@@ -0,0 +1,28 @@
+class Factorial
+  def self.calculate(n)
+    if n < 0
+      raise ArgumentError, "Факторіал не визначений для від'ємних чисел"
+    elsif n == 0
+      return 1
+    else
+      return (1..n).reduce(:*)       # голанг таких приколів ще не бачив...
+    end
+  end
+end
+
+def main
+  puts "Введіть число: "
+  user_input = gets.chomp
+
+  begin
+    number = Integer(user_input)
+    result = Factorial.calculate(number)
+    puts "Факторіал числа #{number} дорівнює #{result} :)"
+  rescue ArgumentError => e
+    puts "Помилка: #{e.message}"
+  rescue
+    puts "Введіть ціле число :)"
+  end
+end
+
+main
